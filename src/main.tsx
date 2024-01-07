68c8046301946ab5b3736bdc6c03a747d288f335
Handoyo Dwi Prasetyo <hdprasetyo00@gmail.com>
2024-01-07 22:22:01 +0700

    tes ci/cd : login-page-admmin

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 // main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LoginAdmin from './LoginAdmin.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoginAdmin from "./LoginAdmin.tsx";
 // import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LoginAdmin />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
