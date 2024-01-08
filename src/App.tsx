512dea08aee5d9fe9d42dd4728e6b808d53ebf7c
ArrasAdazim <arasaladzim98@gmail.com>
2024-01-08 22:50:57 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LoginAdmin from "./pages/Admin/Login/LoginAdmin";
 import Footer from "./components/Footer";
+import RegisterUser from "./pages/User/Register/RegisterUser";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginAdmin />} />
+        <Route path="/register" element={<RegisterUser />} />
         <Route path="/footer" element={<Footer />} />
       </Routes>
     </>
