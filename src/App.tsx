857b870ba461419d7493c8254092698e058e6dc6
andykaadi54 <andykaadi54@gmail.com>
2024-01-10 15:50:25 +0700

    add Filter List Ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginAdmin from './pages/Admin/Login/LoginAdmin'
-import RegisterUser from './pages/User/Register/RegisterUser'
-import Footer from './components/Footer'
-import LoginUser from './pages/User/Login/LoginUser'
-import Header from './components/Header'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import LoginAdmin from "./pages/Admin/Login/LoginAdmin";
+import RegisterUser from "./pages/User/Register/RegisterUser";
+import Footer from "./components/Footer";
+import LoginUser from "./pages/User/Login/LoginUser";
+import Header from "./components/Header";
+import "./App.css";
+import FilterListTicket from "./components/FilterListTicket";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<LoginAdmin />} />
-                <Route path='/header' element={<Header />} />
-                <Route path='/user' element={<LoginUser />} />
-                <Route path='/register' element={<RegisterUser />} />
-                <Route path='/footer' element={<Footer />} />
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginAdmin />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/user" element={<LoginUser />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/filterListTicket" element={<FilterListTicket />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
