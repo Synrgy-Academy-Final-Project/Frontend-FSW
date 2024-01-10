0b2798cc4286c3eb096830295e405d1488cad25c
ArrasAdazim <arasaladzim98@gmail.com>
2024-01-10 23:35:13 +0700

    Integasi Login dan Validasi Login menggunakan yup

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Footer from "./components/Footer";
 import RegisterUser from "./pages/User/Register/RegisterUser";
 import LoginUser from "./pages/User/Login/LoginUser";
 import Header from "./components/Header";
+import "./App.css";
+import FilterListTicket from "./components/FilterListTicket";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginAdmin />} />
-        <Route path='/header' element={<Header />} />
+        <Route path="/header" element={<Header />} />
         <Route path="/user" element={<LoginUser />} />
         <Route path="/register" element={<RegisterUser />} />
         <Route path="/footer" element={<Footer />} />
+        <Route path="/filterListTicket" element={<FilterListTicket />} />
       </Routes>
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,54 @@
-import './Header.css'
+import "./Header.css";
 
 export default function Header() {
-    return (
-        <header>
-            <div className='bg-plane'>
-                <nav>
-                    <ul className='nav-list'>
-                        <li>
-                            <a href='#'>Beranda</a>
-                        </li>
-                        <li>
-                            <a href='#'>Tiket Pesawat</a>
-                        </li>
-                        <li>
-                            <a href='#'>Tempat Populer</a>
-                        </li>
-                        <li>
-                            <a href='#'>Tentang Kami</a>
-                        </li>
-                        <li className='ms-auto login'>
-                            <a href='#'>Masuk</a>
-                        </li>
-                        <li className='register'>
-                            <a href='#'>Daftar</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className='title'>
-                    <h1>Fly.id</h1>
-                    <h3>Dapatkan tiket terbaik untuk perjalanan Anda!</h3>
-                </div>
-            </div>
-        </header>
-    )
+  // Mendapatkan token dari local storage
+  const token = localStorage.getItem("token");
+
+  // Fungsi untuk logout
+  const handleLogout = () => {
+    // Hapus token dari local storage
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
+  return (
+    <header>
+      <div className="bg-plane">
+        <nav>
+          <ul className="nav-list">
+            <li>
+              <a href="#">Beranda</a>
+            </li>
+            <li>
+              <a href="#">Tiket Pesawat</a>
+            </li>
+            <li>
+              <a href="#">Tempat Populer</a>
+            </li>
+            <li>
+              <a href="#">Tentang Kami</a>
+            </li>
+            <li className="ms-auto login">
+              {/* Tampilkan button Masuk atau Logout berdasarkan keberadaan token */}
+              {token ? (
+                <button className="logout" onClick={handleLogout}>
+                  Logout
+                </button>
+              ) : (
+                <a href="#">Masuk</a>
+              )}
+            </li>
+            <li className="register">
+              {/* Tampilkan button Daftar atau tidak ada jika token ada */}
+              {!token && <a href="#">Daftar</a>}
+            </li>
+          </ul>
+        </nav>
+        <div className="title">
+          <h1>Fly.id</h1>
+          <h3>Dapatkan tiket terbaik untuk perjalanan Anda!</h3>
+        </div>
+      </div>
+    </header>
+  );
 }
