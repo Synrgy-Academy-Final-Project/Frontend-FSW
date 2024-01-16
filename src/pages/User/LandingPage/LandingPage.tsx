f54fbf9ff9682f0c751e124f0135eab7f4ddc47c
ArrasAdazim <arasaladzim98@gmail.com>
2024-01-16 23:10:12 +0700

    update

--- src/pages/User/LandingPage/LandingPage.tsx
+++ src/pages/User/LandingPage/LandingPage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
+import Promo from "../../../components/Promo";
+import PopularPlaces from "../../../components/PopularPlaces";
+import AboutUs from "../../../components/AboutUs";
 
 export default function LandingPage() {
   return (
     <>
       <Header />
+      <Promo />
+      <PopularPlaces />
+      <AboutUs />
       <Footer />
     </>
   );
