814328d4852fb42c97d4bd30982f029acbf0083b
Handoyo Dwi Prasetyo <hdprasetyo00@gmail.com>
2024-01-19 19:57:13 +0700

    add component search, detail ticket, modal ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import LoginAdmin from "./pages/Admin/Login/LoginAdmin";
 import Footer from "./components/Footer";
+import TicketSearch from "./components/TicketSearch";
+import ModalPesanTiket from "./components/ModalPesanTiket";
+import DetailTicket from "./components/DetailTicket";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginAdmin />} />
         <Route path="/footer" element={<Footer />} />
+        <Route path="/ticketsearch" element={<TicketSearch />} />
+        <Route path="/modalticket" element={<ModalPesanTiket />} />
+        <Route path="/detailticket" element={<DetailTicket />} />
       </Routes>
     </>
   );
