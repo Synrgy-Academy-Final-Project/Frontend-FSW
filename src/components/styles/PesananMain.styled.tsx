f21ef16e2eb921d0594e0910b21c935f918d2a2d
andykaadi54 <andykaadi54@gmail.com>
2024-02-03 06:29:14 +0700

    add Pesanan Page

---
+++ src/components/styles/PesananMain.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+  width: 1440px;
+  padding: 30px 48px;
+  align-items: flex-start;
+  gap: 48px;
+  background: var(--Neutral-02, #f5f5f5);
+`;
---
+++ src/pages/User/PesananPage/PesananPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PesananHeader from "../../../components/PesananHeader";
+import PesananMain from "../../../components/PesananMain";
+import Footer from "../../../components/Footer";
+
+const PesananPage: React.FC = () => {
+  return (
+    <>
+      <PesananHeader />
+      <PesananMain />
+      <Footer />
+    </>
+  );
+};
+export default PesananPage;
