3472474ce35e7137d4fee6e5db43086c5dcbadd1
Misbahudin <misbahudin.id@gmail.com>
2024-02-12 19:07:27 +0700

    missing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,33 @@
 import { Route, Routes } from 'react-router-dom'
+
+// Admin
+import LoginAdmin from '././pages/Admin/Login/LoginAdmin.tsx'
+import Dashboard from './components/Admin/Dashboard.tsx'
+import IndexBandara from './components/Admin/indexBandara.tsx'
+import IndexMaskapai from './components/Admin/indexMaskapai.tsx'
+import IndexTanggal from './components/Admin/indexTanggal.tsx'
+import IndexTransaksi from './components/Admin/indexTransaksi.tsx'
+import IndexDetailPesawat from './components/Admin/indexDetailPesawat.tsx'
+// user
+import FilterListTicket from './components/FilterListTicket'
+import TicketSearch from './components/TicketSearch'
+import ModalPesanTiket from './components/ModalPesanTiket'
+import DetailTicket from './components/DetailTicket'
+import ETicket from './components/ETicket.tsx'
+import DetailPerjalanan from './components/DetailPerjalanan.tsx'
+import ModalKonfirmasi from './components/ModalKonfirmasi.tsx'
+import FormProfile from './components/FormProfile.tsx'
+import PesananPage from './pages/User/PesananPage/PesananPage.tsx'
+import SidebarAkun from './components/SidebarAkun.tsx'
+import ProfilePage from './pages/User/ProfilePage/ProfilePage.tsx'
+import ProfileEdit from './pages/User/ProfilePage/ProfileEdit/ProfileEdit.tsx'
+import PembayaranPage from './pages/User/PembayaranPage/PembayaranPage.tsx'
+import InformationProfile from './components/InformationProfile.tsx'
+import OTPVerification from './pages/User/ForgetPassword/OTPVerification'
+import DropdownClass from './components/DropdownClass.tsx'
+import DetailSectionPayment from './components/DetailSectionPayment.tsx'
+import DetailHarga from './components/DetailHarga.tsx'
+import DetailSection from './components/DetailSection.tsx'
 import RegisterUser from './pages/User/Register/RegisterUser'
 import LoginUser from './pages/User/Login/LoginUser'
 import DataPenumpang from './pages/User/DataPenumpang/DataPenumpang'
@@ -9,34 +38,6 @@ import VerifyAccount from './pages/User/VerifyAccount/VerifyAccount'
 import ForgetPassword from './pages/User/ForgetPassword/ForgetPassword'
 import ResetPassword from './pages/User/ForgetPassword/ResetPassword'
 import LandingPage from './pages/User/LandingPage/LandingPage'
-// Admin
-import LoginAdmin from '././pages/Admin/Login/LoginAdmin.tsx'
-import Dashboard from './components/Admin/Dashboard.tsx'
-import IndexBandara from './components/Admin/indexBandara.tsx'
-import IndexMaskapai from './components/Admin/indexMaskapai.tsx'
-import IndexTanggal from './components/Admin/indexTanggal.tsx'
-import IndexTransaksi from './components/Admin/indexTransaksi.tsx'
-import IndexDetailPesawat from './components/Admin/indexDetailPesawat.tsx'
-// import FilterListTicket from './components/FilterListTicket'
-import TicketSearch from './components/TicketSearch'
-// import ModalPesanTiket from './components/ModalPesanTiket'
-// import DetailTicket from './components/DetailTicket'
-// import DetailSection from './components/DetailSection'
-// import DetailHarga from './components/DetailHarga'
-// import DropdownClass from './components/DropdownPassenger'
-// import DetailSectionPayment from './components/DetailSectionPayment'
-// import OTPVerification from "./pages/User/ForgetPassword/OTPVerification";
-import ETicket from './components/ETicket.tsx'
-// import DetailPerjalanan from './components/DetailPerjalanan.tsx'
-// import ModalKonfirmasi from './components/ModalKonfirmasi.tsx'
-import FormProfile from './components/FormProfile.tsx'
-import PesananPage from './pages/User/PesananPage/PesananPage.tsx'
-import SidebarAkun from './components/SidebarAkun.tsx'
-import ProfilePage from './pages/User/ProfilePage/ProfilePage.tsx'
-import ProfileEdit from './pages/User/ProfilePage/ProfileEdit/ProfileEdit.tsx'
-import PembayaranPage from './pages/User/PembayaranPage/PembayaranPage.tsx'
-import InformationProfile from './components/InformationProfile.tsx'
-import OTPVerification from './pages/User/ForgetPassword/OTPVerification'
 
 function App() {
   return (
@@ -60,13 +61,18 @@ function App() {
         <Route path='/detailpembayaran' element={<PembayaranPage />} />
 
         {/* Components */}
+        {/* missing props */}
         {/* <Route path="/filterListTicket" element={<FilterListTicket />} /> */}
+        {/* missing props */}
         {/* <Route path="/sidebarakun" element={<SidebarAkun />} /> */}
         <Route path='/eticket-comp' element={<ETicket />} />
+        {/* missing props */}
         {/* <Route path='/detailperjalanan' element={<DetailPerjalanan />} /> */}
+        {/* missing props */}
         {/* <Route path='/modalkonfirmasi' element={<ModalKonfirmasi />} /> */}
         <Route path='/formprofile' element={<FormProfile />} />
         <Route path='/ticketsearch' element={<TicketSearch />} />
+        {/* missing props */}
         {/* <Route path="/modalticket" element={<ModalPesanTiket />} /> */}
 
         {/*Admin*/}
@@ -79,12 +85,17 @@ function App() {
         <Route path='/transaction' element={<IndexTransaksi />} />
         <Route path='/detail-airline/:id' element={<IndexDetailPesawat />} />
         <Route path='/ticketsearch' element={<TicketSearch />} />
+        {/* missing props */}
         {/* <Route path='/detailticket' element={<DetailTicket />} /> */}
+        {/* missing props */}
         {/* <Route path='/detailsection' element={<DetailSection />} /> */}
+        {/* missing props */}
         {/* <Route path='/detailharga' element={<DetailHarga />} /> */}
+        {/* component cardtiket not found */}
         {/* <Route path='/cardtiket' element={<CardTicket />} /> */}
-        {/* <Route path='/cek2' element={<DropdownClass />} /> */}
+        <Route path='/cek2' element={<DropdownClass />} />
         <Route path='/verify-account-forgot' element={<OTPVerification />} />
+        {/* missing props */}
         {/* <Route path='/detailsectionpayment' element={<DetailSectionPayment />} /> */}
       </Routes>
     </>
